05c495568ad1f0897d27a3ed244554cbb219aaa3
CamiloDev404 <crosti1826@gmail.com>
2024-02-17 18:34:31 -0500

    First commit

---
+++ src/main/java/com/predios/PrediosApplication.java
@@ -0,0 +1,13 @@
+package com.predios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrediosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrediosApplication.class, args);
+	}
+
+}
